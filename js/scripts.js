2ca1580e9e6bcf40da179861641d453b443661c1
Karolina <szefu6@wp.pl>
2017-03-09 21:59:37 +0100

    DOM

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// plik scripts.js
+
+var withButtonsClass = document.getElementsByClassName('button');
+
+
+	for (var i=0; i<withButtonsClass.length; i++) {
+		alert(withButtonsClass[i].innerText);
+		console.log(withButtonsClass[i].innerText);
+	}
+
+
+
+
